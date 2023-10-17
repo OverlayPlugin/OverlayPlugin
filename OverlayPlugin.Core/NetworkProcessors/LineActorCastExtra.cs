21d5c817588d476818ecd0554b055fda84adc40f
XP <smurfs@smurfs.verymad.net>
2023-10-16 20:21:11 -0700

    Working cast location/facing logic

--- OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -43,6 +44,7 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
             }
+
             var customLogLines = container.Resolve<FFXIVCustomLogLines>();
             this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
             {
@@ -62,21 +64,35 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 if (*(ushort*)&buffer[offsetMessageType] == packetOpcode)
                 {
-                    object packet = actorCastType.GetConstructor(new Type[] { }).Invoke(new object[] { });
-                    Marshal.PtrToStructure(new IntPtr(buffer), packet);
+                    object packet = Marshal.PtrToStructure(new IntPtr(buffer), actorCastType);
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
+                    // see https://github.com/ravahn/FFXIV_ACT_Plugin/issues/298
+                    // for x/y/x, subtract 7FFF then divide by (2^15 - 1) / 100
+                    float x = ((UInt16)actorCastType.GetField("PosX").GetValue(packet) - 0x7FFF) / 32.767f;
+                    // In-game uses Y as elevation and Z as north-south, but ACT convention is to use
+                    // Z as elevation and Y as north-south.
+                    float y = ((UInt16)actorCastType.GetField("PosZ").GetValue(packet) - 0x7FFF) / 32.767f;
+                    float z = ((UInt16)actorCastType.GetField("PosY").GetValue(packet) - 0x7FFF) / 32.767f;
+                    // for rotation, the packet uses '0' as north, and each increment is 1/65536 of a CCW turn, while
+                    // in-game uses 0=south, pi/2=west, +/-pi=north
+                    // Machina thinks this is a float but that appears to be incorrect, so we have to reinterpret as
+                    // a UInt16
+                    double h = BitConverter.ToUInt16(BitConverter.GetBytes(
+                                   (float)actorCastType.GetField("Rotation").GetValue(packet)), 0)
+                               // Normalize to turns
+                               / 65536.0
+                               // Normalize to radians
+                               * 2 * Math.PI
+                               // Flip from 0=north to 0=south like the game uses
+                               - Math.PI;
 
-                    string someData = "";
+                    string line = string.Format(CultureInfo.InvariantCulture,
+                        "{0:F3}|{1:F3}|{2:F3}|{3:F3}",
+                        x, y, z, h);
 
-                    someData += actorCastType.GetField("PosX") + "|";
-                    someData += actorCastType.GetField("PosY") + "|";
-                    someData += actorCastType.GetField("PosZ") + "|";
-                    someData += actorCastType.GetField("Heading");
-
-                    logWriter(someData, serverTime);
+                    logWriter(line, serverTime);
                 }
             }
         }
-
     }
 }
