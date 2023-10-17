0e932c606cc7bf388f638dd53a59b32da170ad96
XP <smurfs@smurfs.verymad.net>
2023-10-17 13:23:00 -0700

    Regionalization

--- OverlayPlugin.Core/NetworkProcessors/LineAbilityExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineAbilityExtra.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -12,23 +13,125 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
     public class LineAbilityExtra
     {
         public const uint LogFileLineID = 264;
-        private ILogger logger;
-        private int minSize = Int32.MaxValue;
-        private readonly int offsetMessageType;
+        private readonly ILogger logger;
         private readonly FFXIVRepository ffxiv;
-
-        private readonly Type actionEffectHeaderType;
         private readonly Func<string, DateTime, bool> logWriter;
-        private readonly Type headerType;
+        private readonly NetworkParser netHelper;
 
-        private readonly Dictionary<int, ActionEffectTypeInfo> opcodeToType =
-            new Dictionary<int, ActionEffectTypeInfo>();
+        /**
+         * Holds information that may be specific to the current game region.
+         */
+        private RegionalizedInfo regionalized;
 
-        private readonly FieldInfo fieldCastSourceId;
-        private readonly FieldInfo fieldAbilityId;
-        private readonly FieldInfo fieldGlobalEffectCounter;
-        private readonly FieldInfo fieldR;
+        /**
+         * Holds information that may be specific to different game client regions
+         */
+        private class RegionalizedInfo
+        {
+            public readonly Dictionary<int, ActionEffectTypeInfo> opcodeToType =
+                new Dictionary<int, ActionEffectTypeInfo>();
 
+            public int minSize = Int32.MaxValue;
+            public readonly int offsetMessageType;
+            public readonly Type actionEffectHeaderType;
+            public readonly Type headerType;
+
+            public readonly FieldInfo fieldCastSourceId;
+            public readonly FieldInfo fieldAbilityId;
+            public readonly FieldInfo fieldGlobalEffectCounter;
+            public readonly FieldInfo fieldR;
+
+            public RegionalizedInfo(Assembly mach, GameRegion region, ILogger logger, NetworkParser netHelper)
+            {
+                Func<int, string> actionEffectTypesTemplate;
+                switch (region)
+                {
+                    case GameRegion.Global:
+                        headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                        actionEffectHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_ActionEffectHeader");
+                        actionEffectTypesTemplate = i => "Machina.FFXIV.Headers.Server_ActionEffect" + i;
+                        break;
+                    case GameRegion.Chinese:
+                        headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                        actionEffectHeaderType =
+                            mach.GetType("Machina.FFXIV.Headers.Chinese.Server_ActionEffectHeader");
+                        actionEffectTypesTemplate = i => "Machina.FFXIV.Headers.Chinese.Server_ActionEffect" + i;
+                        break;
+                    case GameRegion.Korean:
+                        headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                        actionEffectHeaderType = mach.GetType("Machina.FFXIV.Headers.Korean.Server_ActionEffectHeader");
+                        actionEffectTypesTemplate = i => "Machina.FFXIV.Headers.Korean.Server_ActionEffect" + i;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(region), region, null);
+                }
+
+                fieldCastSourceId = headerType.GetField("ActorID");
+                fieldAbilityId = actionEffectHeaderType.GetField("actionId");
+                fieldR = actionEffectHeaderType.GetField("rotation");
+                fieldGlobalEffectCounter = actionEffectHeaderType.GetField("globalEffectCounter");
+
+                int[] sizes = {1, 8, 16, 24, 32};
+                foreach (int size in sizes)
+                {
+                    Type aeType = mach.GetType(actionEffectTypesTemplate(size));
+                    int machinaTypeSize = Marshal.SizeOf(aeType);
+                    var actionEffectTypeInfo = new ActionEffectTypeInfo()
+                    {
+                        actionEffectSize = size,
+                        packetSize = machinaTypeSize,
+                    };
+                    minSize = Math.Min(machinaTypeSize, minSize);
+                    logger.Log(LogLevel.Debug, "ActionEffect Size {0} -> {1:X4}", size,
+                        actionEffectTypeInfo.packetSize);
+                    // Fortunately, these are currently identical between regions, so there is no need to have separate
+                    // CN/KR versions (yet). If that happens down the line, make copies of them for additional regions,
+                    // and change the switch/case statement to also consider these.
+                    switch (size)
+                    {
+                        case 8:
+                        {
+                            actionEffectTypeInfo.extraType = typeof(Server_ActionEffect8_Extra);
+                            break;
+                        }
+                        case 16:
+                        {
+                            actionEffectTypeInfo.extraType = typeof(Server_ActionEffect16_Extra);
+                            break;
+                        }
+                        case 24:
+                        {
+                            actionEffectTypeInfo.extraType = typeof(Server_ActionEffect24_Extra);
+                            break;
+                        }
+                        case 32:
+                        {
+                            actionEffectTypeInfo.extraType = typeof(Server_ActionEffect32_Extra);
+                            break;
+                        }
+                    }
+
+                    if (size != 1)
+                    {
+                        int extraSize = Marshal.SizeOf(actionEffectTypeInfo.extraType);
+                        if (machinaTypeSize != extraSize)
+                        {
+                            logger.Log(LogLevel.Error, "ActionEffect size mismatch! {} -> {}", machinaTypeSize,
+                                extraSize);
+                            actionEffectTypeInfo.hasError = true;
+                        }
+                    }
+
+                    opcodeToType.Add(netHelper.GetOpcode("Ability" + size), actionEffectTypeInfo);
+                }
+
+                offsetMessageType = netHelper.GetOffset(headerType, "MessageType");
+            }
+        }
+
+        /**
+         * Holds information specific to a particular 'size' of ActionEffect
+         */
         private class ActionEffectTypeInfo
         {
             public int actionEffectSize;
@@ -124,73 +227,33 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
         {
             logger = container.Resolve<ILogger>();
             ffxiv = container.Resolve<FFXIVRepository>();
-            var netHelper = container.Resolve<NetworkParser>();
+            netHelper = container.Resolve<NetworkParser>();
+            if (!ffxiv.IsFFXIVPluginPresent())
+                return;
+            ffxiv.RegisterNetworkParser(MessageReceived);
+            ffxiv.RegisterProcessChangedHandler(ProcessChanged);
+
+            var customLogLines = container.Resolve<FFXIVCustomLogLines>();
+            logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
+            {
+                Name = "ActorCastExtra",
+                Source = "OverlayPlugin",
+                ID = LogFileLineID,
+                Version = 1,
+            });
+        }
+
+        // Load regionalized data when the game region changes
+        private void ProcessChanged(Process process)
+        {
+            GameRegion region = ffxiv.GetMachinaRegion();
             if (!ffxiv.IsFFXIVPluginPresent())
                 return;
             try
             {
-                var mach = Assembly.Load("Machina.FFXIV");
-                var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                fieldCastSourceId = headerType.GetField("ActorID");
-
-                actionEffectHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_ActionEffectHeader");
-                fieldAbilityId = actionEffectHeaderType.GetField("actionId");
-                fieldR = actionEffectHeaderType.GetField("rotation");
-                fieldGlobalEffectCounter = actionEffectHeaderType.GetField("globalEffectCounter");
-
-                int[] sizes = { 1, 8, 16, 24, 32 };
-                foreach (int size in sizes)
-                {
-                    Type aeType = mach.GetType("Machina.FFXIV.Headers.Server_ActionEffect" + size);
-                    int machinaTypeSize = Marshal.SizeOf(aeType);
-                    var actionEffectTypeInfo = new ActionEffectTypeInfo()
-                    {
-                        actionEffectSize = size,
-                        packetSize = machinaTypeSize,
-                    };
-                    minSize = Math.Min(machinaTypeSize, minSize);
-                    logger.Log(LogLevel.Debug, "ActionEffect Size {0} -> {1:X4}", size,
-                        actionEffectTypeInfo.packetSize);
-                    switch (size)
-                    {
-                        case 8:
-                            {
-                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect8_Extra);
-                                break;
-                            }
-                        case 16:
-                            {
-                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect16_Extra);
-                                break;
-                            }
-                        case 24:
-                            {
-                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect24_Extra);
-                                break;
-                            }
-                        case 32:
-                            {
-                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect32_Extra);
-                                break;
-                            }
-                    }
-
-                    if (size != 1)
-                    {
-                        int extraSize = Marshal.SizeOf(actionEffectTypeInfo.extraType);
-                        if (machinaTypeSize != extraSize)
-                        {
-                            logger.Log(LogLevel.Error, "ActionEffect size mismatch! {} -> {}", machinaTypeSize,
-                                extraSize);
-                            actionEffectTypeInfo.hasError = true;
-                        }
-                    }
-                    opcodeToType.Add(netHelper.GetOpcode("Ability" + size), actionEffectTypeInfo);
-                }
-
-                offsetMessageType = netHelper.GetOffset(msgHeaderType, "MessageType");
-                ffxiv.RegisterNetworkParser(MessageReceived);
+                Assembly mach = Assembly.Load("Machina.FFXIV");
+                RegionalizedInfo info = new RegionalizedInfo(mach, region, logger, netHelper);
+                regionalized = info;
             }
             catch (System.IO.FileNotFoundException)
             {
@@ -200,27 +263,22 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
             }
-
-            var customLogLines = container.Resolve<FFXIVCustomLogLines>();
-            this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
-            {
-                Name = "ActorCastExtra",
-                Source = "OverlayPlugin",
-                ID = LogFileLineID,
-                Version = 1,
-            });
         }
 
         private unsafe void MessageReceived(string id, long epoch, byte[] message)
         {
-            if (message.Length < minSize)
+            RegionalizedInfo regInfo = regionalized;
+            if (regInfo == null)
+                return;
+
+            if (message.Length < regInfo.minSize)
                 return;
 
             fixed (byte* buffer = message)
             {
-                int opCode = *(ushort*)&buffer[offsetMessageType];
+                int opCode = *(ushort*)&buffer[regInfo.offsetMessageType];
                 ActionEffectTypeInfo info;
-                bool result = opcodeToType.TryGetValue(opCode, out info);
+                bool result = regInfo.opcodeToType.TryGetValue(opCode, out info);
                 if (result)
                 {
                     if (message.Length < info.packetSize)
@@ -230,14 +288,14 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
 
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
 
-                    object header = Marshal.PtrToStructure(new IntPtr(buffer), headerType);
-                    UInt32 sourceId = (UInt32)fieldCastSourceId.GetValue(header);
+                    object header = Marshal.PtrToStructure(new IntPtr(buffer), regInfo.headerType);
+                    UInt32 sourceId = (UInt32)regInfo.fieldCastSourceId.GetValue(header);
 
-                    object aeHeader = Marshal.PtrToStructure(new IntPtr(buffer), actionEffectHeaderType);
+                    object aeHeader = Marshal.PtrToStructure(new IntPtr(buffer), regInfo.actionEffectHeaderType);
                     // Ability ID is really 16-bit, so it is formatted as such, but we will get an
                     // exception if we try to prematurely cast it to UInt16
-                    UInt32 abilityId = (UInt32)fieldAbilityId.GetValue(aeHeader);
-                    UInt32 globalEffectCounter = (UInt32)fieldGlobalEffectCounter.GetValue(aeHeader);
+                    UInt32 abilityId = (UInt32)regInfo.fieldAbilityId.GetValue(aeHeader);
+                    UInt32 globalEffectCounter = (UInt32)regInfo.fieldGlobalEffectCounter.GetValue(aeHeader);
 
                     if (info.actionEffectSize == 1)
                     {
@@ -264,7 +322,7 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
                     float y = ffxiv.ConvertUInt16Coordinate(aeExtra.y);
                     float z = ffxiv.ConvertUInt16Coordinate(aeExtra.z);
 
-                    double h = ffxiv.ConvertHeading((ushort)fieldR.GetValue(aeHeader));
+                    double h = ffxiv.ConvertHeading((ushort)regInfo.fieldR.GetValue(aeHeader));
                     string line = string.Format(CultureInfo.InvariantCulture,
                         "{0:X8}|{1:X4}|{2:X8}|{3}|{4:F3}|{5:F3}|{6:F3}|{7:F3}",
                         sourceId, abilityId, globalEffectCounter, (int)LineSubType.DATA_PRESENT, x, y, z, h);
--- OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -9,34 +10,26 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
     {
         public const uint LogFileLineID = 263;
         private ILogger logger;
-        private readonly int packetSize;
-        private readonly int packetOpcode;
-        private readonly int offsetMessageType;
         private readonly FFXIVRepository ffxiv;
-        private readonly Type headerType;
-        private readonly Type actorCastType;
-        private readonly FieldInfo fieldCastSourceId;
-        private readonly FieldInfo fieldAbilityId;
-        private readonly FieldInfo fieldX;
-        private readonly FieldInfo fieldY;
-        private readonly FieldInfo fieldZ;
-        private readonly FieldInfo fieldR;
 
-        private readonly Func<string, DateTime, bool> logWriter;
-
-        public LineActorCastExtra(TinyIoCContainer container)
+        private class RegionalizedInfo
         {
-            logger = container.Resolve<ILogger>();
-            ffxiv = container.Resolve<FFXIVRepository>();
-            var netHelper = container.Resolve<NetworkParser>();
-            if (!ffxiv.IsFFXIVPluginPresent())
-                return;
-            try
+            public readonly int packetSize;
+            public readonly int packetOpcode;
+            public readonly int offsetMessageType;
+            public readonly Type headerType;
+            public readonly Type actorCastType;
+            public readonly FieldInfo fieldCastSourceId;
+            public readonly FieldInfo fieldAbilityId;
+            public readonly FieldInfo fieldX;
+            public readonly FieldInfo fieldY;
+            public readonly FieldInfo fieldZ;
+            public readonly FieldInfo fieldR;
+
+            public RegionalizedInfo(Type headerType, Type actorCastType, NetworkParser netHelper)
             {
-                var mach = Assembly.Load("Machina.FFXIV");
-                var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
-                actorCastType = mach.GetType("Machina.FFXIV.Headers.Server_ActorCast");
+                this.headerType = headerType;
+                this.actorCastType = actorCastType;
                 fieldCastSourceId = headerType.GetField("ActorID");
                 fieldAbilityId = actorCastType.GetField("ActionID");
                 fieldX = actorCastType.GetField("PosX");
@@ -45,8 +38,69 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
                 fieldR = actorCastType.GetField("Rotation");
                 packetOpcode = netHelper.GetOpcode("ActorCast");
                 packetSize = Marshal.SizeOf(actorCastType);
-                offsetMessageType = netHelper.GetOffset(msgHeaderType, "MessageType");
-                ffxiv.RegisterNetworkParser(MessageReceived);
+                offsetMessageType = netHelper.GetOffset(headerType, "MessageType");
+            }
+        }
+
+        private RegionalizedInfo regionalized;
+
+        private readonly Func<string, DateTime, bool> logWriter;
+        private readonly NetworkParser netHelper;
+
+        public LineActorCastExtra(TinyIoCContainer container)
+        {
+            logger = container.Resolve<ILogger>();
+            ffxiv = container.Resolve<FFXIVRepository>();
+            netHelper = container.Resolve<NetworkParser>();
+            ffxiv.RegisterNetworkParser(MessageReceived);
+            ffxiv.RegisterProcessChangedHandler(ProcessChanged);
+
+            var customLogLines = container.Resolve<FFXIVCustomLogLines>();
+            logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
+            {
+                Name = "ActorCastExtra",
+                Source = "OverlayPlugin",
+                ID = LogFileLineID,
+                Version = 1,
+            });
+        }
+
+        private void ProcessChanged(Process process)
+        {
+            GameRegion region = ffxiv.GetMachinaRegion();
+            if (!ffxiv.IsFFXIVPluginPresent())
+                return;
+            try
+            {
+                Assembly mach = Assembly.Load("Machina.FFXIV");
+                Type headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                string actorCastTypeStr;
+                switch (region)
+                {
+                    case GameRegion.Global:
+                    {
+                        actorCastTypeStr = "Machina.FFXIV.Headers.Server_ActorCast";
+                        break;
+                    }
+                    case GameRegion.Korean:
+                    {
+                        actorCastTypeStr = "Machina.FFXIV.Headers.Korean.Server_ActorCast";
+                        break;
+                    }
+                    case GameRegion.Chinese:
+                    {
+                        actorCastTypeStr = "Machina.FFXIV.Headers.Chinese.Server_ActorCast";
+                        break;
+                    }
+                    default:
+                    {
+                        return;
+                    }
+                }
+
+                Type actorCastType = mach.GetType(actorCastTypeStr);
+                RegionalizedInfo info = new RegionalizedInfo(headerType, actorCastType, netHelper);
+                regionalized = info;
             }
             catch (System.IO.FileNotFoundException)
             {
@@ -56,44 +110,38 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
             }
-
-            var customLogLines = container.Resolve<FFXIVCustomLogLines>();
-            this.logWriter = customLogLines.RegisterCustomLogLine(new LogLineRegistryEntry()
-            {
-                Name = "ActorCastExtra",
-                Source = "OverlayPlugin",
-                ID = LogFileLineID,
-                Version = 1,
-            });
         }
 
         private unsafe void MessageReceived(string id, long epoch, byte[] message)
         {
-            if (message.Length < packetSize)
+            RegionalizedInfo info = regionalized;
+            if (info == null)
+                return;
+
+            if (message.Length < info.packetSize)
                 return;
 
             fixed (byte* buffer = message)
             {
-                if (*(ushort*)&buffer[offsetMessageType] == packetOpcode)
+                if (*(ushort*)&buffer[info.offsetMessageType] == info.packetOpcode)
                 {
-                    object header = Marshal.PtrToStructure(new IntPtr(buffer), headerType);
-                    UInt32 sourceId = (UInt32)fieldCastSourceId.GetValue(header);
+                    object header = Marshal.PtrToStructure(new IntPtr(buffer), info.headerType);
+                    UInt32 sourceId = (UInt32)info.fieldCastSourceId.GetValue(header);
 
-                    object packet = Marshal.PtrToStructure(new IntPtr(buffer), actorCastType);
-                    UInt16 abilityId = (UInt16)fieldAbilityId.GetValue(packet);
+                    object packet = Marshal.PtrToStructure(new IntPtr(buffer), info.actorCastType);
+                    UInt16 abilityId = (UInt16)info.fieldAbilityId.GetValue(packet);
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
                     // for x/y/x, subtract 7FFF then divide by (2^15 - 1) / 100
-                    float x = ffxiv.ConvertUInt16Coordinate((UInt16)fieldX.GetValue(packet));
+                    float x = ffxiv.ConvertUInt16Coordinate((UInt16)info.fieldX.GetValue(packet));
                     // In-game uses Y as elevation and Z as north-south, but ACT convention is to use
                     // Z as elevation and Y as north-south.
-                    float y = ffxiv.ConvertUInt16Coordinate((UInt16)fieldZ.GetValue(packet));
-                    float z = ffxiv.ConvertUInt16Coordinate((UInt16)fieldY.GetValue(packet));
+                    float y = ffxiv.ConvertUInt16Coordinate((UInt16)info.fieldZ.GetValue(packet));
+                    float z = ffxiv.ConvertUInt16Coordinate((UInt16)info.fieldY.GetValue(packet));
                     // for rotation, the packet uses '0' as north, and each increment is 1/65536 of a CCW turn, while
                     // in-game uses 0=south, pi/2=west, +/-pi=north
                     // Machina thinks this is a float but that appears to be incorrect, so we have to reinterpret as
                     // a UInt16
-                    double h = ffxiv.ConvertHeading(ffxiv.InterpretFloatAsUInt16((float)fieldR.GetValue(packet)));
-
+                    double h = ffxiv.ConvertHeading(ffxiv.InterpretFloatAsUInt16((float)info.fieldR.GetValue(packet)));
 
 
                     string line = string.Format(CultureInfo.InvariantCulture,
