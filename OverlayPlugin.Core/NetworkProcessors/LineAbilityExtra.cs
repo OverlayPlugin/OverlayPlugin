374d9910a5d3a00be13e4719c944466c9e6a2cbf
XP <smurfs@smurfs.verymad.net>
2023-10-17 10:33:31 -0700

    Add extra fields, refactor

--- OverlayPlugin.Core/NetworkProcessors/LineAbilityExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineAbilityExtra.cs
@@ -24,15 +24,106 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
         private readonly int offsetMessageType;
         private readonly FFXIVRepository ffxiv;
 
-        private Type actionEffectHeaderType;
-        private Func<string, DateTime, bool> logWriter;
-        private Dictionary<int, ActionEffectTypeInfo> opcodeToType = new Dictionary<int, ActionEffectTypeInfo>();
-        private FieldInfo fieldR;
+        private readonly Type actionEffectHeaderType;
+        private readonly Func<string, DateTime, bool> logWriter;
+        private readonly Type headerType;
+
+        private readonly Dictionary<int, ActionEffectTypeInfo> opcodeToType =
+            new Dictionary<int, ActionEffectTypeInfo>();
+
+        private readonly FieldInfo fieldCastSourceId;
+        private readonly FieldInfo fieldGlobalEffectCounter;
+        private readonly FieldInfo fieldR;
 
         private class ActionEffectTypeInfo
         {
+            public int actionEffectSize;
             public int packetSize;
-            public Type machinaType;
+            public Type extraType;
+            public bool hasError = false;
+        }
+
+        interface IActionEffectExtra
+        {
+            ushort x { get; }
+            ushort y { get; }
+            ushort z { get; }
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 0x29C)]
+        private struct Server_ActionEffect8_Extra : IActionEffectExtra
+        {
+            [FieldOffset(0x290)]
+            private ushort _x;
+
+            [FieldOffset(0x292)]
+            private ushort _z;
+
+            [FieldOffset(0x294)]
+            private ushort _y;
+
+            public ushort x => _x;
+            public ushort y => _y;
+            public ushort z => _z;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 0x4DC)]
+        private struct Server_ActionEffect16_Extra : IActionEffectExtra
+        {
+            [FieldOffset(0x4D0)]
+            public ushort _x;
+
+            [FieldOffset(0x4D2)]
+            public ushort _z;
+
+            [FieldOffset(0x4D4)]
+            public ushort _y;
+
+            public ushort x => _x;
+            public ushort y => _y;
+            public ushort z => _z;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 0x71C)]
+        private struct Server_ActionEffect24_Extra : IActionEffectExtra
+        {
+            [FieldOffset(0x710)]
+            public ushort _x;
+
+            [FieldOffset(0x712)]
+            public ushort _z;
+
+            [FieldOffset(0x714)]
+            public ushort _y;
+
+            public ushort x => _x;
+            public ushort y => _y;
+            public ushort z => _z;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 0x95C)]
+        private struct Server_ActionEffect32_Extra : IActionEffectExtra
+        {
+            [FieldOffset(0x950)]
+            public ushort _x;
+
+            [FieldOffset(0x952)]
+            public ushort _z;
+
+            [FieldOffset(0x954)]
+            public ushort _y;
+
+            public ushort x => _x;
+            public ushort y => _y;
+            public ushort z => _z;
+        }
+
+        // For possible future expansion.
+        private enum LineSubType
+        {
+            NO_DATA = 0,
+            DATA_PRESENT = 1,
+            ERROR = 256
         }
 
         public LineAbilityExtra(TinyIoCContainer container)
@@ -46,19 +137,54 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 var mach = Assembly.Load("Machina.FFXIV");
                 var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                fieldCastSourceId = headerType.GetField("ActorID");
+
                 actionEffectHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_ActionEffectHeader");
                 fieldR = actionEffectHeaderType.GetField("rotation");
+                fieldGlobalEffectCounter = actionEffectHeaderType.GetField("globalEffectCounter");
 
-                int[] sizes = {1, 8, 16, 24, 32};
+                int[] sizes = { 1, 8, 16, 24, 32 };
                 foreach (int size in sizes)
                 {
                     Type aeType = mach.GetType("Machina.FFXIV.Headers.Server_ActionEffect" + size);
+                    int machinaTypeSize = Marshal.SizeOf(aeType);
                     var actionEffectTypeInfo = new ActionEffectTypeInfo()
                     {
-                        machinaType = aeType,
-                        packetSize = Marshal.SizeOf(aeType)
+                        actionEffectSize = size,
+                        packetSize = machinaTypeSize,
                     };
-                    minSize = Math.Min(actionEffectTypeInfo.packetSize, minSize);
+                    minSize = Math.Min(machinaTypeSize, minSize);
+                    logger.Log(LogLevel.Debug, "ActionEffect Size {0} -> {1:X4}", size, actionEffectTypeInfo.packetSize);
+                    switch (size)
+                    {
+                        case 8:
+                            {
+                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect8_Extra);
+                                break;
+                            }
+                        case 16:
+                            {
+                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect16_Extra);
+                                break;
+                            }
+                        case 24:
+                            {
+                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect24_Extra);
+                                break;
+                            }
+                        case 32:
+                            {
+                                actionEffectTypeInfo.extraType = typeof(Server_ActionEffect32_Extra);
+                                break;
+                            }
+                    }
+                    int extraSize = Marshal.SizeOf(actionEffectTypeInfo.extraType);
+                    if (machinaTypeSize != extraSize)
+                    {
+                        logger.Log(LogLevel.Error, "ActionEffect size mismatch! {} -> {}", machinaTypeSize, extraSize);
+                        actionEffectTypeInfo.hasError = true;
+                    }
                     opcodeToType.Add(netHelper.GetOpcode("Ability" + size), actionEffectTypeInfo);
                 }
 
@@ -92,28 +218,51 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             fixed (byte* buffer = message)
             {
                 int opCode = *(ushort*)&buffer[offsetMessageType];
-                var info = opcodeToType[opCode];
-                if (info != null)
+                ActionEffectTypeInfo info;
+                bool result = opcodeToType.TryGetValue(opCode, out info);
+                if (result)
                 {
                     if (message.Length < info.packetSize)
                     {
                         return;
                     }
-                    object packet = Marshal.PtrToStructure(new IntPtr(buffer), actionEffectHeaderType);
+
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                    float x = (BitConverter.ToUInt16(message, message.Length - 12) - 0x7FFF) / 32.767f;
-                    float z = (BitConverter.ToUInt16(message, message.Length - 10) - 0x7FFF) / 32.767f;
-                    float y = (BitConverter.ToUInt16(message, message.Length - 8) - 0x7FFF) / 32.767f;
-                    double h = ((ushort)fieldR.GetValue(packet))
-                               // Normalize to turns
-                               / 65536.0
-                               // Normalize to radians
-                               * 2 * Math.PI
-                               // Flip from 0=north to 0=south like the game uses
-                               - Math.PI;
+
+                    object header = Marshal.PtrToStructure(new IntPtr(buffer), headerType);
+                    UInt32 sourceId = (UInt32)fieldCastSourceId.GetValue(header);
+
+                    object aeHeader = Marshal.PtrToStructure(new IntPtr(buffer), actionEffectHeaderType);
+                    UInt32 globalEffectCounter = (UInt32)fieldGlobalEffectCounter.GetValue(aeHeader);
+
+                    if (info.actionEffectSize == 1)
+                    {
+                        // AE1 is not useful. It does not contain this data. But we still need to write something
+                        // to indicate that a proper line will not be happening.
+                        logWriter(string.Format(CultureInfo.InvariantCulture,
+                            "{0:X8}|{1:X8}|{2}||||",
+                            sourceId, globalEffectCounter, (int)LineSubType.NO_DATA), serverTime);
+                        return;
+                    }
+
+                    if (info.hasError)
+                    {
+                        logWriter(string.Format(CultureInfo.InvariantCulture,
+                            "{0:X8}|{1:X8}|{2}||||",
+                            sourceId, globalEffectCounter, (int)LineSubType.ERROR), serverTime);
+                        return;
+                    }
+
+                    IActionEffectExtra aeExtra = (IActionEffectExtra)Marshal.PtrToStructure(new IntPtr(buffer), info.extraType);
+
+                    float x = ffxiv.ConvertUInt16Coordinate(aeExtra.x);
+                    float y = ffxiv.ConvertUInt16Coordinate(aeExtra.y);
+                    float z = ffxiv.ConvertUInt16Coordinate(aeExtra.z);
+
+                    double h = ffxiv.ConvertHeading((ushort)fieldR.GetValue(aeHeader));
                     string line = string.Format(CultureInfo.InvariantCulture,
-                        "{0:F3}|{1:F3}|{2:F3}|{3:F3}",
-                        x, y, z, h);
+                        "{0:X8}|{1:X8}|{2}|{3:F3}|{4:F3}|{5:F3}|{6:F3}",
+                        sourceId, globalEffectCounter, (int)LineSubType.DATA_PRESENT, x, y, z, h);
 
                     logWriter(line, serverTime);
                 }
--- OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineActorCastExtra.cs
@@ -13,13 +13,15 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
         private readonly int packetOpcode;
         private readonly int offsetMessageType;
         private readonly FFXIVRepository ffxiv;
+        private readonly Type headerType;
         private readonly Type actorCastType;
+        private readonly FieldInfo fieldCastSourceId;
         private readonly FieldInfo fieldX;
         private readonly FieldInfo fieldY;
         private readonly FieldInfo fieldZ;
         private readonly FieldInfo fieldR;
 
-        private Func<string, DateTime, bool> logWriter;
+        private readonly Func<string, DateTime, bool> logWriter;
 
         public LineActorCastExtra(TinyIoCContainer container)
         {
@@ -32,7 +34,9 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 var mach = Assembly.Load("Machina.FFXIV");
                 var msgHeaderType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
+                headerType = mach.GetType("Machina.FFXIV.Headers.Server_MessageHeader");
                 actorCastType = mach.GetType("Machina.FFXIV.Headers.Server_ActorCast");
+                fieldCastSourceId = headerType.GetField("ActorID");
                 fieldX = actorCastType.GetField("PosX");
                 fieldY = actorCastType.GetField("PosY");
                 fieldZ = actorCastType.GetField("PosZ");
@@ -70,31 +74,28 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 if (*(ushort*)&buffer[offsetMessageType] == packetOpcode)
                 {
+                    object header = Marshal.PtrToStructure(new IntPtr(buffer), headerType);
+                    UInt32 sourceId = (UInt32)fieldCastSourceId.GetValue(header);
+
                     object packet = Marshal.PtrToStructure(new IntPtr(buffer), actorCastType);
                     DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                    // see https://github.com/ravahn/FFXIV_ACT_Plugin/issues/298
                     // for x/y/x, subtract 7FFF then divide by (2^15 - 1) / 100
-                    float x = ((UInt16)fieldX.GetValue(packet) - 0x7FFF) / 32.767f;
+                    float x = ffxiv.ConvertUInt16Coordinate((UInt16)fieldX.GetValue(packet));
                     // In-game uses Y as elevation and Z as north-south, but ACT convention is to use
                     // Z as elevation and Y as north-south.
-                    float y = ((UInt16)fieldZ.GetValue(packet) - 0x7FFF) / 32.767f;
-                    float z = ((UInt16)fieldY.GetValue(packet) - 0x7FFF) / 32.767f;
+                    float y = ffxiv.ConvertUInt16Coordinate((UInt16)fieldZ.GetValue(packet));
+                    float z = ffxiv.ConvertUInt16Coordinate((UInt16)fieldY.GetValue(packet));
                     // for rotation, the packet uses '0' as north, and each increment is 1/65536 of a CCW turn, while
                     // in-game uses 0=south, pi/2=west, +/-pi=north
                     // Machina thinks this is a float but that appears to be incorrect, so we have to reinterpret as
                     // a UInt16
-                    double h = BitConverter.ToUInt16(BitConverter.GetBytes(
-                                   (float)fieldR.GetValue(packet)), 0)
-                               // Normalize to turns
-                               / 65536.0
-                               // Normalize to radians
-                               * 2 * Math.PI
-                               // Flip from 0=north to 0=south like the game uses
-                               - Math.PI;
+                    double h = ffxiv.ConvertHeading(ffxiv.InterpretFloatAsUInt16((float)fieldR.GetValue(packet)));
+
+
 
                     string line = string.Format(CultureInfo.InvariantCulture,
-                        "{0:F3}|{1:F3}|{2:F3}|{3:F3}",
-                        x, y, z, h);
+                        "{0:X8}|{1:F3}|{2:F3}|{3:F3}|{4:F3}",
+                        sourceId, x, y, z, h);
 
                     logWriter(line, serverTime);
                 }
